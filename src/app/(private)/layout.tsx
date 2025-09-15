2099d988c63ef327fbb7719037f2d2b51665fb61
GUTZ333 <gutz333lpsking32jotavve18@hotmail.com>
2025-09-14 21:00:38 -0300

    style(Dashboard): :art: Adjuste sidebar header

--- src/app/(private)/layout.tsx
+++ src/app/(private)/layout.tsx
@@ -30,10 +30,10 @@ export default function PrivateLayout({
       >
         <NextThemeProvider>
           <AppSideBarProvider>
-            <AppSidebar collapsible="icon" variant="floating" />
+            <AppSidebar collapsible="icon" variant="inset" />
             <SidebarInset>
-              <header className="flex h-(--header-height) shrink-0 items-center gap-2 border-b transition-[width,height] ease-linear group-has-data-[collapsible=icon]/sidebar-wrapper:h-(--header-height) p-3">
-                <div className="flex w-full items-center gap-1 px-4 lg:gap-2 lg:px-6">
+              <header className="flex h-(--header-height) shrink-0 items-center gap-2 border-b transition-[width,height] ease-linear group-has-data-[collapsible=icon]/sidebar-wrapper:h-(--header-height)">
+                <div className="flex w-full items-center gap-1 px-4 lg:gap-2 p-3">
                   <SidebarTrigger className="-ml-1" />
                   <Separator
                     orientation="vertical"
